868cbc155e85d2a3726ac50fc26aaf3461bc62f7
fred8728 <fskn@hotmail.com>
2019-11-12 13:45:58 +0100

    added/changed security

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -51,13 +51,13 @@ public class User implements Serializable {
 
   //TODO Change when password is hashed
    public boolean verifyPassword(String pw){
-        return(pw.equals(userPass));
+        return (BCrypt.checkpw(pw, userPass));
     }
 
   public User(String userName, String userPass) {
     this.userName = userName;
 
-    this.userPass = userPass;
+     this.userPass = BCrypt.hashpw(userPass, BCrypt.gensalt() );
   }
 
 
