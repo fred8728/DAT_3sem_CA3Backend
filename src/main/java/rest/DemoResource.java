c4f681476da716f266d62252e6a3d52f7d336018
A O <ahmedothman8@hotmail.com>
2019-11-13 13:06:04 +0100

    created a rest endpoint for SW person as a copy blueprint for future work

--- src/main/java/rest/DemoResource.java
+++ src/main/java/rest/DemoResource.java
@@ -2,7 +2,12 @@ package rest;
 
 import com.google.gson.Gson;
 import entities.User;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
+import java.util.Scanner;
 import javax.annotation.security.RolesAllowed;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -11,6 +16,7 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
 import utils.EMF_Creator;
@@ -67,4 +73,22 @@ public class DemoResource {
         String thisuser = securityContext.getUserPrincipal().getName();
         return "{\"msg\": \"Hello to (admin) User: " + thisuser + "\"}";
     }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("person/{id}")  
+    public String getSwPeopleData(@PathParam("id") int id) throws MalformedURLException, IOException{
+    URL url = new URL("https://swapi.co/api/people/"+id);
+    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("GET");
+    con.setRequestProperty("Accept", "application/json;charset=UTF-8");
+    con.setRequestProperty("User-Agent", "server"); //remember if you are using SWAPI
+    Scanner scan = new Scanner(con.getInputStream());
+    String jsonStr = null;
+    if (scan.hasNext()) {
+      jsonStr = scan.nextLine();
+      //jsonStr += "\n";
+    }
+    scan.close();
+    return jsonStr;
+  }
 }
